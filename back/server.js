bbda22fd9a95c76cbbe5eb7ed7afdd9ac754fbef
fymmit <blomqvist.timo@gmail.com>
2019-02-16 19:02:46 +0200

    Add image sending.

--- back/server.js
+++ back/server.js
@@ -5,6 +5,20 @@ app.get('/', (req, res) => {
     res.send('Hello world!')
 })
 
+app.get('/image/:name', (req, res) => {
+    let options = {
+        root: __dirname + '/photos/',
+        headers: {
+            'x-timestamp': Date.now(),
+            'x-sent': true
+        }
+    }
+    let fileName = req.params.name
+    res.sendFile(fileName, options, err => {
+        err ? res.send(err.message) : console.log('Sent:', fileName)
+    })
+})
+
 app.listen(port, () => {
     console.log('Listening on port', port)
 })
