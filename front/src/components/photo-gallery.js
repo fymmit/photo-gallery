ea850cf9b71f5f574192bdb53ad3301ac3d5907b
fymmit <blomqvist.timo@gmail.com>
2019-02-25 17:34:02 +0200

    Reverse image order.

--- front/src/components/photo-gallery.js
+++ front/src/components/photo-gallery.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Photo from './photo';
 
 const PhotoGallery = (props) => {
-    const images = props.images.map(image => {
+    const images = props.images.reverse().map(image => {
         return(
             <Photo 
                 name={image}/>
