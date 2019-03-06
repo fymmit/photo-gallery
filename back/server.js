8545f6005608f12374bd0130ef33a627411774e5
fymmit <blomqvist.timo@gmail.com>
2019-03-06 08:51:43 +0200

    Stop users from uploading images with broken extensions.

--- back/server.js
+++ back/server.js
@@ -3,6 +3,7 @@ const app = express()
 const cors = require('cors')
 const bodyParser = require('body-parser')
 const fileUpload = require('express-fileupload')
+const fs = require('fs')
 const port = 9000
 const files = require('./file-handler.js')
 const db = require('./db.js')
@@ -32,18 +33,35 @@ app.post('/images', (req, res) => {
         return res.status(400).send('No files were uploaded.');
     }
     let file = req.files.image
-    let newName = `${Date.now()}${file.name.substring(file.name.lastIndexOf('.'))}`
-    path = __dirname + '/photos/' + newName
-    file.mv(path, async (err) => {
-        if (err) return res.status(500).send(err)
-        let fileType = await files.detectFileType(path)
-        if (fileType && (fileType.mime == 'image/jpeg' || fileType.mime == 'image/png')) {
-            db.insertImageNames([newName])
-        } else {
-            files.deleteFile(path)
-        }
-    })
-    res.redirect('back')
+    if (file.mimetype == 'image/jpeg' || file.mimetype == 'image/png') {
+        let newName = `${Date.now()}`
+        path = __dirname + '/photos/' + newName
+        file.mv(path, async (err) => {
+            if (err) return res.status(500).send(err)
+            let fileType = await files.detectFileType(path)
+            switch (fileType.ext) {
+                case 'jpg': {
+                    newName += '.jpg'
+                    fs.rename(path, path + '.jpg', (err) => {
+                        if (err) console.log(err)
+                    })
+                } case 'png': {
+                    newName += '.png'
+                    fs.rename(path, path + '.png', (err) => {
+                        if (err) console.log(err)
+                    })
+                }
+            }
+            if (fileType && (fileType.mime == 'image/jpeg' || fileType.mime == 'image/png')) {
+                db.insertImageNames([newName])
+            } else {
+                files.deleteFile(path)
+            }
+        })
+        res.redirect('back')
+    } else {
+        res.send('Try with an image.')
+    }
 })
 
 app.delete('/images', (req, res) => {
