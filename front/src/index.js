27ba2142aeae9b979166c39dc599f73f442fd1c5
fymmit <blomqvist.timo@gmail.com>
2019-07-20 10:14:34 +0300

    ReactGA

--- front/src/index.js
+++ front/src/index.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import ReactGA from 'react-ga';
+
+ReactGA.initialize('UA-144205394-1');
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 ReactDOM.render(<App />, document.getElementById('root'));
